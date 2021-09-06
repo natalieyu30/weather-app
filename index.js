7a470969f739654a40e551fe1f8af4ab67e379a7
Natalie Yu <natalieyu30@gmail.com>
2021-09-06 09:04:51 -0400

    add cors

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ const categoryRoute = require("./routes/categoriesRoute");
 const multer = require("multer");
 const path = require("path");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 app.use(express.json());
+app.use(cors());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose
